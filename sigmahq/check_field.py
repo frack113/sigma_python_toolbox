1ccef8b438ce7dcfcb60987c223eac56cc866240
frack113 <62423083+frack113@users.noreply.github.com>
2021-11-15 20:16:09 +0100

    Split windows and add ETW

--- sigmahq/check_field.py
+++ sigmahq/check_field.py
@@ -28,93 +28,142 @@ import logging
 
 class DataBase():
 
-    def __init__(self,filename):
-        self.filename = filename
-        if pathlib.Path(filename).exists():
-            with pathlib.Path(filename).open('r',encoding='UTF-8') as file:
+    def __init__(self,filename_dft,filename_win):
+        self.filename_dft = filename_dft
+        if pathlib.Path(filename_dft).exists():
+            with pathlib.Path(filename_dft).open('r',encoding='UTF-8') as file:
                 yml_rule = ruamel.yaml.load(file, Loader=ruamel.yaml.RoundTripLoader)
-            self.data = yml_rule
+            self.data_dft = yml_rule
         else:
-            self.data = {}
+            self.data_dft = {}
+        self.filename_win =filename_win
+        if pathlib.Path(filename_win).exists():
+            with pathlib.Path(filename_win).open('r',encoding='UTF-8') as file:
+                yml_rule = ruamel.yaml.load(file, Loader=ruamel.yaml.RoundTripLoader)
+            self.data_win = yml_rule
+        else:
+            self.data_win = {}
     
-    def update(self,product,category,service,eventid,state,value):
-        if eventid == None:
-            order_list = self.data[product][category][service][state]
-            order_list.append(value)
-            order_list = sorted(set(order_list))
-            self.data[product][category][service][state] = order_list
-        else:
-            order_list = self.data[product][category][service][eventid][state]
-            order_list.append(value)
-            order_list = sorted(set(order_list))
-            self.data[product][category][service][eventid][state] = order_list           
-    
-    def create(self,product,category,service,eventid = None):
-        if product not in self.data.keys():
-            self.data[product] = {}
-        if category not in self.data[product].keys():
-            self.data[product][category] = {}
-        if service not in self.data[product][category].keys():
-            self.data[product][category][service] = {}
-        if eventid == None:
-            self.data[product][category][service] = {"valid":[],"rule":[]}
-        else:
-            if eventid not in self.data[product][category][service].keys():
-                self.data[product][category][service][eventid] = {"valid":[],"rule":[]}
+    def update_dft(self,state,value,product,category,service):
+        order_list = self.data_dft[product][category][service][state]
+        order_list.append(value)
+        order_list = sorted(set(order_list))
+        self.data_dft[product][category][service][state] = order_list
 
+    def update_win(self,state,value,product,category,service,etw,eventid):
+        order_list = self.data_win[product][category][service][etw][eventid][state]
+        order_list.append(value)
+        order_list = sorted(set(order_list))
+        self.data_win[product][category][service][etw][eventid][state] = order_list
 
-    def check(self,logsource,value):
+    def update(self,state,value,product,category,service,etw=None,eventid=None):
+        if eventid == None:
+            self.update_dft(state,value,product,category,service)
+        else:
+            self.update_win(state,value,product,category,service,etw,eventid)
+ 
+    def create_dft(self,product,category,service):
+        if product not in self.data_dft.keys():
+            self.data_dft[product] = {}
+        if category not in self.data_dft[product].keys():
+            self.data_dft[product][category] = {}
+        if service not in self.data_dft[product][category].keys():
+            self.data_dft[product][category][service] = {"valid":[],"rule":[]}
+
+    def create_win(self,product,category,service,etw,eventid):
+        if product not in self.data_win.keys():
+            self.data_win[product] = {}
+        if category not in self.data_win[product].keys():
+            self.data_win[product][category] = {}
+        if service not in self.data_win[product][category].keys():
+            self.data_win[product][category][service] = {}
+        if etw not in self.data_win[product][category][service].keys():
+            self.data_win[product][category][service][etw] = {}
+        if eventid not in self.data_win[product][category][service][etw].keys():
+            self.data_win[product][category][service][etw][eventid] = {"valid":[],"rule":[]}
+
+    def create(self,product,category,service,etw = None,eventid = None):
+        if eventid == None:
+            self.create_dft(product,category,service)
+        else:
+            self.create_win(product,category,service,etw,eventid)
+
+    def check_dft(self,logsource,value):
         product = logsource["product"]
-        if product in self.data.keys():
-            category = logsource["category"]
-            if category in self.data[product].keys():
-                service = logsource["service"]
-                if service in self.data[product][category].keys():
-                    eventid = logsource["eventid"]
-                    if eventid == None:
-                        if value in self.data[product][category][service]["valid"]:
-                            return True
-                        else:
-                            self.update(product,category,service,None,"rule",value)
+        category = logsource["category"]
+        service = logsource["service"]
+        if product in self.data_dft.keys():
+            if category in self.data_dft[product].keys():
+                if service in self.data_dft[product][category].keys():
+                    if value in self.data_dft[product][category][service]["valid"]:
+                        return True
                     else:
-                        if eventid in self.data[product][category][service].keys():
-                            if value in self.data[product][category][service][eventid]["valid"]:
-                                return True
-                            else:
-                              self.update(product,category,service,eventid,"rule",value)
-                        else:
-                            self.create(product,category,service,eventid)
+                        self.update("rule",value,product,category,service)
                 else: #service
-                    if product == "windows" and category == "None" :
-                        self.create(product,category,service,"any")
-                    else:
-                        self.create(product,category,service)         
+                    self.create(product,category,service)         
             else: # category
-                self.create(product,category,"None")
+                self.create(product,category,service)
         else: # product
-            self.create(product,"None","None")
+            self.create(product,category,service)
         return False
 
-    def update_default(self):
-        if "windows" in self.data.keys():  #error 1st lanch with with data empty 
-            if "None" in self.data["windows"]:
-                if "None" in self.data["windows"]["None"].keys():
-                    self.data["windows"]["None"].pop("None",None)
+    def check_win(self,logsource,value):
+        product = logsource["product"]
+        category = logsource["category"]
+        service = logsource["service"]
+        eventid = logsource["eventid"]
+        if product in self.data_win.keys():
+            if category in self.data_win[product].keys():
+                if service in self.data_win[product][category].keys():
+                    valid = False
+                    for etw in self.data_win[product][category][service]:
+                        if eventid in self.data_win[product][category][service][etw].keys():
+                            if value in self.data_win[product][category][service][etw][eventid]["valid"]:
+                                valid = True
+                    if not valid:
+                        if value in self.data_win[product][category][service]["default"]["0"]["valid"]:
+                            valid = True
+                    
+                    if valid:
+                        return True
+                    else:
+                        self.create(product,category,service,"default",eventid) 
+                        self.update("rule",value,product,category,service,"default",eventid)
+                else: #service
+                    self.create(product,category,service,"default",eventid)         
+            else: # category
+                self.create(product,category,service,"default",eventid)
+        else: # product
+            self.create(product,category,service,"default",eventid)
+        return False
 
-                for service in self.data["windows"]["None"]:
-                    if not "any" in self.data["windows"]["None"][service].keys():
-                        self.create("windows","None",service,"any")
-                        self.update("windows","None",service,"any","valid","EventID")
-                        self.update("windows","None",service,"any","valid","Provider_Name")
-                    if len(self.data["windows"]["None"][service])>1:
-                        valid =[]
-                        for eventid in self.data["windows"]["None"][service]:
-                            valid.extend(self.data["windows"]["None"][service][eventid]["valid"])
-                        self.data["windows"]["None"][service]["any"]["valid"] = sorted(set(valid))
+    def check(self,logsource,value):
+        if logsource["product"] == "windows":
+            return self.check_win(logsource,value)
+        else:
+            return self.check_dft(logsource,value)
+
+    def clean_rule(self,mydict):
+        for k in mydict.keys():
+            value = mydict[k]
+            if isinstance(value,dict):
+                if "rule" in value.keys():
+                    mydict[k]["rule"] = []
+                else:
+                    value = self.clean_rule(value)
+    
+    def init_win_default(self):
+        for category in self.data_win["windows"]:
+            for service in self.data_win["windows"][category]:
+                self.create_win("windows",category,service,"default","0")
+                self.update("valid","EventID","windows",category,service,"default","0")
+                self.update("valid","provider_name","windows",category,service,"default","0")
 
     def save(self):
-        with pathlib.Path(self.filename).open('w',encoding='UTF-8') as file:
-            ruamel.yaml.dump(self.data, file, Dumper=ruamel.yaml.RoundTripDumper,indent=4,block_seq_indent=4)
+        with pathlib.Path(self.filename_dft).open('w',encoding='UTF-8') as file:
+            ruamel.yaml.dump(self.data_dft, file, Dumper=ruamel.yaml.RoundTripDumper,indent=4,block_seq_indent=4)
+        with pathlib.Path(self.filename_win).open('w',encoding='UTF-8') as file:
+            ruamel.yaml.dump(self.data_win, file, Dumper=ruamel.yaml.RoundTripDumper,indent=4,block_seq_indent=4)
 
 class MySigma():
 
@@ -173,14 +222,14 @@ class MySigma():
                                     elif isinstance(detection_event_id,int):
                                         event_id = str(detection_event_id)
             self.index = f'{self.logsource["product"]}_{self.logsource["category"]}_{self.logsource["service"]}'
-            if self.logsource["product"] == "windows" and self.logsource["category"] == "None":
+            if self.logsource["product"] == "windows":
                 if unique:
                     if event_id == None:
-                        self.logsource["eventid"] = "any"
+                        self.logsource["eventid"] = "0"
                     else:
                         self.logsource["eventid"] = event_id
                 else:
-                    self.logsource["eventid"] = "any"
+                    self.logsource["eventid"] = "0"
                 self.index = f'{self.index}_{self.logsource["eventid"]}'
 
 class OSSEM_DD():
@@ -198,7 +247,7 @@ class OSSEM_DD():
                 service = yml_zeek["event_code"]
                 self.database.create("zeek","None",service)
                 for field in yml_zeek["event_fields"]:
-                    self.database.update("zeek","None",service,None,"valid",str(field["name"]))
+                    self.database.update("valid",str(field["name"]),"zeek","None",service)
         zeek_bar.close()
 
     def update_windows(self,rep,product,category,service):
@@ -209,10 +258,10 @@ class OSSEM_DD():
             with win_file.open('r',encoding='UTF-8') as file:
                 yml_win = ruamel.yaml.load(file, Loader=ruamel.yaml.RoundTripLoader)
                 eventid = yml_win["event_code"]
-               # etw = yml_win["log_source"]
-                self.database.create(product,category,service,eventid)
+                etw = yml_win["log_source"]
+                self.database.create(product,category,service,etw,eventid)
                 for field in yml_win["event_fields"]:
-                    self.database.update(product,category,service,eventid,"valid",str(field["name"]).replace(" ","_"))
+                    self.database.update("valid",str(field["name"]).replace(" ","_"),product,category,service,etw,eventid)
         win_bar.close()
 
     def update_cloud(self,rep):
@@ -221,16 +270,16 @@ class OSSEM_DD():
         for cloud_file in cloud_list:
             cloud_bar.update(1)
             with cloud_file.open('r',encoding='UTF-8') as file:
-                try:
-                    yml_cloud = ruamel.yaml.load(file, Loader=ruamel.yaml.RoundTripLoader)
-                except:
-                    continue
+                #try:
+                yml_cloud = ruamel.yaml.load(file, Loader=ruamel.yaml.RoundTripLoader)
+                #except:
+                #    continue
                 product = yml_cloud["platform"]
                 category = "None"
                 service = yml_cloud["event_code"]
                 self.database.create(product,category,service)
                 for field in yml_cloud["event_fields"]:
-                    self.database.update(product,category,service,None,"valid",str(field["name"]).replace(" ","_").replace(":","_"))
+                    self.database.update("valid",str(field["name"]).replace(" ","_").replace(":","_"),product,category,service)
         cloud_bar.close()
 
 def order_dict(mydict):
@@ -253,8 +302,12 @@ path_ossemdb = args.ossemdb
 output_yml = args.rapport
 
 
-print ("Load database")
-info = DataBase("check_field.yml")
+print("Load database")
+info = DataBase("check_field_dft.yml","check_field_win.yml")
+print("Clean old bad rule field name")
+info.clean_rule(info.data_dft)
+info.clean_rule(info.data_win)
+
 
 print ("Load OSSEM-DB")
 ossem = OSSEM_DD(path_ossemdb,info)
@@ -264,10 +317,15 @@ ossem.update_cloud("azure")
 ossem.update_windows("Microsoft-Windows-Security-Auditing","windows","None","security")
 ossem.update_windows("Microsoft-Windows-AppLocker","windows","None","applocker")
 ossem.update_windows("Microsoft-Windows-SMBClient","windows","None","smbclient-security")
+ossem.update_windows("Microsoft-Windows-NTLM","windows","None","ntlm")
+ossem.update_windows("Microsoft-Windows-Dhcp-Client","windows","None","dhcp")
+ossem.update_windows("Microsoft-Windows-DriverFrameworks-UserMode","windows","None","driver-framework")
+ossem.update_windows("Microsoft-Windows-PrintService","windows","None","printservice-admin")  # ?
+ossem.update_windows("Microsoft-Windows-PrintService","windows","None","printservice-operational") # ?
+ossem.update_windows("Microsoft-Windows-SMBClient","windows","None","smbclient-security") # ?
 
-
-print("Update default field")
-info.update_default()
+print("Create default etw")
+info.init_win_default()
 
 print("Processing :")
 invalid = {}
@@ -291,7 +349,8 @@ for sigma_file in sigma_list:
 sigma_bar.close()
 
 print("Save database")
-info.data = order_dict(info.data)
+info.data_dft = order_dict(info.data_dft)
+info.data_win = order_dict(info.data_win)
 info.save()
 
 print(f"find {len(invalid)} elements to be considered")
